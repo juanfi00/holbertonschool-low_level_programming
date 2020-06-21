663f2fc8cf6a92441df20146003673f92dbd364a
juanfi00 <juanfi00@hotmail.com>
2020-06-21 17:51:36 +0000

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,8 @@
 /* puts will print strings.*/
 /*return (0) will end with value 0.*/
 
-int main(void){
-
-puts("\"Programming is like building a multilingual puzzle");
-
-  return(0);
-
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
